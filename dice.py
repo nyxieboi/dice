96046835f4c16f0968d06ade8c919e635702425a
Nora Abu Shamleh <nora.abushamleh@skola.botkyrka.se>
2019-11-18 10:08:39 +0100

    First commit

---
+++ dice.py
@@ -0,0 +1,13 @@
+import random
+
+print("Welcome to my dicesimulator")
+
+while True:
+    diceiboi = random.randint(1,6)
+    print(str("rolling dice."))
+        
+    
+    
+    print("The dice rolled and you got: ", diceiboi)
+   
+    #input("Press" enter? tf "to roll the dice again or" q "to exit the program")
